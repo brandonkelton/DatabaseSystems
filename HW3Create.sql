368d13459bb7c71bd25e9371caca9f3f9873f6ab
Brandon Kelton <30930160+brandonkelton@users.noreply.github.com>
2017-11-05 14:22:34 -0700

    New version of Homework 3
    I completely re-imagined how to apply 3NF to homework 3 data.

---
+++ HW3Create.sql
@@ -0,0 +1,206 @@
+CREATE TABLE aircraft_config
+(
+    aircraft_config_id smallint NOT NULL,
+    CONSTRAINT aircraft_config_pkey PRIMARY KEY (aircraft_config_id)
+);
+
+CREATE TABLE aircraft_type
+(
+    aircraft_type_id integer NOT NULL,
+    aircraft_group smallint NOT NULL,
+    CONSTRAINT aircraft_type_pkey PRIMARY KEY (aircraft_type_id)
+);
+
+CREATE TABLE airline
+(
+    airline_id integer NOT NULL,
+    carrier varchar(3) NOT NULL,
+    carrier_name varchar(255) NOT NULL,
+    entity varchar(50) NOT NULL,
+    region varchar(1) NOT NULL,
+    CONSTRAINT carrier_pkey PRIMARY KEY (airline_id)
+);
+
+CREATE TABLE airline_airline_group
+(
+    airline_id integer NOT NULL,
+    group_id smallint NOT NULL,
+    active_group boolean NOT NULL DEFAULT false,
+    CONSTRAINT airline_carrier_group_pkey PRIMARY KEY (airline_id, group_id),
+    CONSTRAINT fk_airline_airline_group_airline_id FOREIGN KEY (airline_id)
+        REFERENCES public.airline (airline_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_airline_airline_group_group_id FOREIGN KEY (group_id)
+        REFERENCES public.airline_group (group_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE airline_group
+(
+    group_id smallint NOT NULL,
+    CONSTRAINT carrier_group_pkey PRIMARY KEY (group_id)
+);
+
+CREATE TABLE airport
+(
+    airport_id integer NOT NULL,
+    airport_seq_id integer NOT NULL,
+    airport_code varchar(5) NOT NULL,
+    location_id integer,
+    CONSTRAINT airport_pkey PRIMARY KEY (airport_id),
+    CONSTRAINT fk_airport_location_id FOREIGN KEY (location_id)
+        REFERENCES public.airport_location (location_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE airport_departures
+(
+    airport_id integer NOT NULL,
+    scheduled integer NOT NULL DEFAULT 0,
+    performed integer NOT NULL DEFAULT 0,
+    CONSTRAINT airport_departures_pkey PRIMARY KEY (airport_id),
+    CONSTRAINT fk_airport_departures_airport_id FOREIGN KEY (airport_id)
+        REFERENCES public.airport (airport_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE airport_location
+(
+    location_id serial NOT NULL,
+    wac smallint NOT NULL,
+    state_name varchar(50) NOT NULL,
+    state_abr varchar(2) NOT NULL,
+    state_fips smallint NOT NULL,
+    city_name varchar(50) NOT NULL,
+    city_market_id integer NOT NULL,
+    CONSTRAINT airport_location_pkey PRIMARY KEY (location_id),
+    UNIQUE (city_name, state_name)
+);
+
+CREATE TABLE path
+(
+    path_id serial NOT NULL,
+    origin_airport_id integer NOT NULL,
+    destination_airport_id integer NOT NULL,
+    distance integer NOT NULL,
+    UNIQUE (origin_airport_id, destination_airport_id, distance),
+    CONSTRAINT trip_pkey PRIMARY KEY (path_id),
+    CONSTRAINT fk_path_destination_airport_id FOREIGN KEY (destination_airport_id)
+        REFERENCES public.airport (airport_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_path_origin_airport_id FOREIGN KEY (origin_airport_id)
+        REFERENCES public.airport (airport_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE path_data
+(
+    path_data_id serial NOT NULL,
+    airline_id integer NOT NULL,
+    passengers integer NOT NULL,
+    freight integer NOT NULL,
+    mail integer NOT NULL,
+    air_time integer NOT NULL,
+    seats integer NOT NULL,
+    payload integer NOT NULL,
+    "class" varchar(1),
+    path_id integer,
+    ramp_distance_id integer,
+    aircraft_type_id integer,
+    aircraft_config_id integer,
+    CONSTRAINT flight_pkey PRIMARY KEY (path_data_id),
+    CONSTRAINT fk_path_data_aircraft_config_id FOREIGN KEY (aircraft_config_id)
+        REFERENCES public.aircraft_config (aircraft_config_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_path_data_aircraft_type_id FOREIGN KEY (aircraft_type_id)
+        REFERENCES public.aircraft_type (aircraft_type_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_path_data_airline_id FOREIGN KEY (airline_id)
+        REFERENCES public.airline (airline_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_path_data_path_id FOREIGN KEY (path_id)
+        REFERENCES public.path (path_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_path_data_ramp_distance_id FOREIGN KEY (ramp_distance_id)
+        REFERENCES public.ramp_distance (ramp_distance_id) 
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE ramp_distance
+(
+    ramp_distance_id integer NOT NULL,
+    CONSTRAINT ramp_distance_pkey PRIMARY KEY (ramp_distance_id)
+);
+
+--Function that will be used to load data from CSV file
+--NOTE: The below function is not my code
+
+CREATE OR REPLACE FUNCTION public.load_csv_file(
+	target_table text,
+	csv_path text,
+	col_count integer)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 0
+AS $BODY$
+
+declare
+
+iter integer; -- dummy integer to iterate columns with
+col text; -- variable to keep the column name at each iteration
+col_first text; -- first column name, e.g., top left corner on a csv file or spreadsheet
+
+begin
+    set schema 'public';
+
+    create table temp_table ();
+
+    -- add just enough number of columns
+    for iter in 1..col_count
+    loop
+        execute format('alter table temp_table add column col_%s text;', iter);
+    end loop;
+
+    -- copy the data from csv file
+    execute format('copy temp_table from %L with delimiter '','' quote ''"'' csv ', csv_path);
+
+    iter := 1;
+    col_first := (select col_1 from temp_table limit 1);
+
+    -- update the column names based on the first row which has the column names
+    for col in execute format('select unnest(string_to_array(trim(temp_table::text, ''()''), '','')) from temp_table where col_1 = %L', col_first)
+    loop
+        execute format('alter table temp_table rename column col_%s to %s', iter, col);
+        iter := iter + 1;
+    end loop;
+
+    -- delete the columns row
+    execute format('delete from temp_table where %s = %L', col_first, col_first);
+
+    -- change the temp table name to the name given as parameter, if not blank
+    if length(target_table) > 0 then
+        execute format('alter table temp_table rename to %I', target_table);
+    end if;
+
+end;
+
+$BODY$;
+
+ALTER FUNCTION public.load_csv_file(text, text, integer)
+    OWNER TO postgres;
+
+
---
+++ HW3Load.sql
@@ -0,0 +1,122 @@
+--Raw Data Load
+--Automatically creates table public."rawAirlineData" based on column names and column count in the file referenced below
+--An exact number of columns must be provided to the function
+select load_csv_file('rawAirlineData', 'D:\CSDB\T100D\544193580_T_T100D_SEGMENT_US_CARRIER_ONLY_2017_6.csv', 45);
+
+--Insert data into tables from raw data table
+
+--INSERT: aircraft_config
+insert into aircraft_config (select distinct aircraft_config::smallint from public."rawAirlineData");
+
+--INSERT: aircraft_type
+insert into aircraft_type (aircraft_type_id, aircraft_group) (select distinct aircraft_type::int, aircraft_group::smallint from public."rawAirlineData");
+
+--INSERT: airline_group
+insert into airline_group
+select distinct airline_group::smallint from 
+	(select carrier_group airline_group from public."rawAirlineData" union select carrier_group_new airline_group from public."rawAirlineData") g;
+
+--INSERT: airline
+insert into airline (airline_id, carrier, carrier_name, entity, region)
+select distinct airline_id::int, carrier, carrier_name, unique_carrier_entity, region from public."rawAirlineData";
+
+--INSERT: airline_airline_group
+insert into airline_airline_group (airline_id, group_id, active_group)
+select distinct airline_id::int, airline_group::smallint, active_group::boolean from
+(
+	select airline_id, carrier_group as airline_group, (case when carrier_group = carrier_group_new then true else false end) as active_group from public."rawAirlineData"
+    union
+    select airline_id, carrier_group_new as airline_group, true as active_group from public."rawAirlineData" where not carrier_group = carrier_group_new
+) airline_groups order by airline_id;
+
+--INSERT: airport_location
+insert into airport_location (wac, state_name, state_abr, state_fips, city_name, city_market_id)
+select distinct wac::smallint, state_name, state_abr, state_fips::smallint, city_name, city_market_id::int from
+(
+	select 
+    	origin_wac as wac, 
+    	origin_state_nm as state_name, 
+    	origin_state_abr as state_abr, 
+    	origin_state_fips as state_fips, 
+    	split_part(origin_city_name, ', ', 1) as city_name, 
+    	origin_city_market_id as city_market_id 
+    from 
+    	public."rawAirlineData"
+    union
+    select 
+    	dest_wac as wac, 
+    	dest_state_nm as state_name, 
+    	dest_state_abr as state_abr, 
+    	dest_state_fips as state_fips, 
+    	split_part(dest_city_name, ', ', 1) as city_name, 
+    	dest_city_market_id as city_market_id
+    from 
+    	public."rawAirlineData"
+) data
+
+--INSERT: airport
+insert into airport (airport_id, airport_seq_id, airport_code, location_id)
+select distinct airport_id::int, airport_seq_id::int, airport_code, location_id from
+(
+	select 
+    	origin_airport_id as airport_id, 
+    	origin_airport_seq_id as airport_seq_id, 
+    	origin as airport_code, 
+    	(select location_id from airport_location where city_name=split_part(r.origin_city_name, ', ', 1) and state_name=r.origin_state_nm limit 1) as location_id 
+    from 
+    	public."rawAirlineData" r
+    union
+    select 
+    	dest_airport_id as airport_id, 
+    	dest_airport_seq_id as airport_seq_id, 
+    	dest as airport_code, 
+    	(select location_id from airport_location where city_name=split_part(r.dest_city_name, ', ', 1) and state_name=r.dest_state_nm limit 1) as location_id 
+    from 
+    	public."rawAirlineData" r
+) airports;
+
+--INSERT: airport_departures
+insert into airport_departures (airport_id, scheduled, performed)
+select airports.airport_id::int, coalesce(sum(r.departures_scheduled::int), 0)::int as scheduled, coalesce(sum(r.departures_performed::int), 0)::int as performed
+from (select origin_airport_id as airport_id from public."rawAirlineData" union select dest_airport_id as airport_id from public."rawAirlineData") airports
+left join public."rawAirlineData" r on airports.airport_id = r.origin_airport_id
+group by airports.airport_id;
+
+--INSERT: ramp_distance
+insert into ramp_distance (select distinct ramp_to_ramp::int from public."rawAirlineData");
+
+--INSERT: path
+insert into "path" (origin_airport_id, destination_airport_id, distance)
+select distinct origin_airport_id::int, dest_airport_id::int, distance::int from public."rawAirlineData";
+
+--INSERT: path_data
+insert into path_data
+(
+    path_id,
+	airline_id,
+    passengers,
+    freight,
+    mail,
+    air_time,
+    seats,
+    payload,
+    "class",
+    ramp_distance_id,
+    aircraft_type_id,
+    aircraft_config_id
+)
+select distinct
+	(select path_id from path where origin_airport_id=r.origin_airport_id::int and destination_airport_id=r.dest_airport_id::int and distance=r.distance::int limit 1) as path_id,
+    r.airline_id::int,
+    r.passengers::int,
+    r.freight::int,
+    r.mail::int,
+    r.air_time::int,
+    r.seats::int,
+    r.payload::int,
+    r."class",
+    r.ramp_to_ramp::int,
+    r.aircraft_type::int,
+    r.aircraft_config::int
+from
+	public."rawAirlineData" r
