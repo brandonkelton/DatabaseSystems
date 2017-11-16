6cf1d323beb4cfc360620324ce4266dd3eaa1a56
Brandon Kelton <30930160+brandonkelton@users.noreply.github.com>
2017-11-15 19:04:46 -0700

    Fixed incorrect query
    Adjusted query to pull all flights from Colorado, rather than just Denver

--- HW4/Query1.sql
+++ HW4/Query1.sql
@@ -8,7 +8,7 @@ join airport a on p.origin_airport_id = a.airport_id
 join airport_location l on a.location_id=l.location_id
 join path_data d on p.path_id=d.path_id
 join airline al on d.airline_id=al.airline_id
-where l.city_name='Denver' and l.state_name='Colorado'
+where l.state_name='Colorado'
 group by l.city_name, l.state_name, a.airport_code, al.carrier_name
 
 
